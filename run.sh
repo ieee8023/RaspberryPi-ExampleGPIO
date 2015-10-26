9a38bbfd5a6651db592b6795ae434b8c142c4c13
ieee8023 <joseph@josephpcohen.com>
2015-10-26 11:19:18 -0400

    simple script

--- run.sh
+++ run.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-
-# specify the full path if you link to this script from the scratch script
-#DIR=/home/pi/RaspberryPi-ExampleGPIO
-DIR=.
-
-export DISPLAY=:0.0
-echo $DIR
-cd $DIR
 sudo java -Xmx128m -cp `sh getclasspath.sh`:classes WalkTurtleDemo
 
 
